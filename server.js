8d0c23050bb3cea9b69b56d1b6eb818f5a0b6fd4
Ducanh11108 <140966454+Ducanh11108@users.noreply.github.com>
2025-07-12 23:39:29 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const OpenAI = require('openai');
+
+const app = express();
+
+const openai = new OpenAI({
+    apiKey: process.env.GEMINI_API_KEY,
+    baseURL: process.env.BASEURL
+});
+
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post('/api/chat', async (req, res) => {
+    try {
+        const { messages } = req.body;
+
+        const additionalMessage = {
+            role: 'system',
+            content: 'You are a helpfull chatbot, your responsibility is to answer all user questions in a humorous way and in Vietnamese language, if user need to find more information, you will introduce them to visit: zalo: 0339.195.193 Or facebook link: https://www.facebook.com/ducanhdevlt'
+        };
+
+        const response = await openai.chat.completions.create({
+            model: 'gemini-2.0-flash',
+            messages: [additionalMessage, ...messages],
+        });
+        res.json(response.choices[0].message);
+    } catch (error) {
+        console.error('Error in /api/chat:', error);
+        res.status(500).json({ error: 'An error occurred while processing the request' });
+    }
+});
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => console.log(`Server listening on port ${port}`));
